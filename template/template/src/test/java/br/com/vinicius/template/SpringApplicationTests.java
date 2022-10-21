2de67c1b3ebd2cc9e86bdff98ca0c5bd9979565d
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-10-21 11:08:42 -0300

    add template to spring application

---
+++ template/template/src/test/java/br/com/vinicius/template/SpringApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.vinicius.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
