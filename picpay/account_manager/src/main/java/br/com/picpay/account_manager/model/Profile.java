03896609ff26976d33cf83151a218380d827f065
Vinicius Frota <viniciuspxfrota@gmail.com>
2022-10-27 23:30:31 -0300

    create Messages and Authorization Service, add increase and decrease methods in Profile model

--- picpay/account_manager/src/main/java/br/com/picpay/account_manager/model/Profile.java
+++ picpay/account_manager/src/main/java/br/com/picpay/account_manager/model/Profile.java
@@ -1,5 +1,7 @@
 package br.com.picpay.account_manager.model;
 
+import br.com.picpay.account_manager.exception.InvalidTransactionException;
+import br.com.picpay.account_manager.exception.NotFoundException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -32,4 +34,25 @@ public class Profile {
 
     @Column(nullable = false)
     private BigDecimal balance;
+
+    public void increaseAmount(BigDecimal value) {
+//        Profile profile = profileRepository.findById(payee).orElseThrow( () ->new NotFoundException("Perfil não encontrado"));
+        this.setBalance(this.getBalance().add(value));
+//        profileRepository.save(profile);
+    }
+
+    public void decreaseAmount(BigDecimal value) {
+//        Profile profile = profileRepository.findById(payer).orElseThrow( );;
+
+        if (this.getBalance().compareTo(value) < 0)
+            throw new InvalidTransactionException("Saldo insuficiente");
+
+        this.setBalance(this.getBalance().subtract(value));
+//        profileRepository.save(profile);
+    }
+
+    public boolean isAbleToTransferMoney() {
+//        Profile profile = profileRepository.findById(profileId).orElseThrow( () ->new NotFoundException("Perfil não encontrado"));
+        return !this.isShopKeeper();
+    }
 }
--- picpay/account_manager/src/main/java/br/com/picpay/account_manager/service/TransactionService.java
+++ picpay/account_manager/src/main/java/br/com/picpay/account_manager/service/TransactionService.java
@@ -1,11 +1,14 @@
 package br.com.picpay.account_manager.service;
 
 import br.com.picpay.account_manager.exception.InvalidTransactionException;
+import br.com.picpay.account_manager.exception.NotFoundException;
 import br.com.picpay.account_manager.exception.RequestFailedException;
 import br.com.picpay.account_manager.model.Profile;
 import br.com.picpay.account_manager.model.dto.*;
+import br.com.picpay.account_manager.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Objects;
@@ -13,59 +16,37 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class TransactionService {
-    ProfileService profileService;
-    RestTemplate restTemplate;
+    private final MessagesService messagesService;
+    private final ProfileRepository profileRepository;
+    private final RestTemplate restTemplate;
+    private final AuthorizationService authorizationService;
 
     @Autowired
-    TransactionService(ProfileService profileService, RestTemplate restTemplate) {
-        this.profileService = profileService;
+    TransactionService(ProfileRepository profileRepository, RestTemplate restTemplate,
+                       AuthorizationService authorizationService, MessagesService messagesService) {
+        this.profileRepository = profileRepository;
         this.restTemplate = restTemplate;
+        this.authorizationService = authorizationService;
+        this.messagesService = messagesService;
     }
+
+    @Transactional
     public void makeTransaction(TransactionDTO transaction) {
-        if (!profileService.isAbleToTransferMoney(transaction.getPayer()))
+        Profile payeeProfile = profileRepository.findById(transaction.getPayee())
+                .orElseThrow(() ->new NotFoundException("Perfil não encontrado"));
+
+        Profile payerProfile = profileRepository.findById(transaction.getPayer())
+                .orElseThrow(() ->new NotFoundException("Perfil não encontrado"));
+
+        if (!payerProfile.isAbleToTransferMoney())
             throw new InvalidTransactionException("Este tipo de usuário não pode transferir dinheiro.");
 
-        AuthorizationResponseDTO response;
-        try {
-            response = restTemplate.postForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6",
-                    transaction, AuthorizationResponseDTO.class).getBody();
-        }
-        catch (Exception e) {
-            throw new RequestFailedException("Não foi possível validar a transação.");
-        }
-
-        if (Objects.equals(response.getMessage(), "Autorizado")) {
-            profileService.decreaseAmount(transaction.getPayer(),transaction.getValue());
-            profileService.increaseAmount(transaction.getPayee(), transaction.getValue());
+        if (authorizationService.isAuthorized(transaction)) {
+            payerProfile.decreaseAmount(transaction.getValue());
+            payeeProfile.increaseAmount(transaction.getValue());
         }
         else throw new InvalidTransactionException("Não autorizado");
 
-        CompletableFuture.runAsync(()-> sendTransactionMessage(transaction));
-    }
-
-    public void sendTransactionMessage(TransactionDTO transaction) {
-        ResponseProfileDTO payer = profileService.getProfile(transaction.getPayer());
-        ResponseProfileDTO payee = profileService.getProfile(transaction.getPayee());
-        EmailServiceRequestDTO request = new EmailServiceRequestDTO(payee.getEmail(),
-                "Você recebeu R$" + transaction.getValue() + " do " + payer.getName());
-
-
-        EmailServiceResponseDTO response;
-        try {
-            response = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify",
-                    request, EmailServiceResponseDTO.class,
-                    EmailServiceResponseDTO.class).getBody();
-
-            if (!response.getMessage().equals("Success"))
-                throw new RuntimeException("Serviço externo não conseguiu enviar a mensagem");
-            System.out.println("Mensagem enviada com sucesso");
-        } catch (Exception e) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
-            sendTransactionMessage(transaction);
-        }
+        CompletableFuture.runAsync(()-> messagesService.sendTransactionMessage(transaction));
     }
 }
